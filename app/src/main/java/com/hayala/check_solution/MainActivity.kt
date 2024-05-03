1d07c4b4c899f5218993858a6f77e697c8b2dbc6
hayala124 <elnur7803@gmail.com>
2024-05-03 23:16:02 +0300

    небольшие изменения

--- app/src/main/java/com/hayala/check_solution/MainActivity.kt
+++ app/src/main/java/com/hayala/check_solution/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.hayala.check_solution
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -15,8 +14,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var buttonRight: Button
     private lateinit var buttonWrong: Button
     private lateinit var textResult: TextView
+    private lateinit var txtRightAnswer: TextView
+    private lateinit var txtWrongAnswer: TextView
     private var number_1 = 0
     private var number_2 = 0
+    private var right = 0
+    private var wrong = 0
     private var operation = ' '
     private var probability = true
     private var result = 0
@@ -28,10 +31,13 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
         buttonRight = binding.btnRight
         buttonWrong = binding.btnWrong
         buttonStart = binding.btnStart
         textResult = binding.txtResult
+        txtRightAnswer = binding.txtNumberRight
+        txtWrongAnswer = binding.txtNumberWrong
 
         buttonStart.setOnClickListener {
             val (number1, number2, operation) = onButtonStartPressed()
@@ -39,12 +45,10 @@ class MainActivity : AppCompatActivity() {
             this.number_2 = number2
             this.operation = operation
         }
-        /*buttonRight.setOnClickListener { onButtonRightPressed() }
-        buttonWrong.setOnClickListener { onButtonWrongPressed() }*/
+        buttonRight.setOnClickListener { onButtonRightPressed() }
+        buttonWrong.setOnClickListener { onButtonWrongPressed() }
     }
 
-
-    @SuppressLint("SetTextI18n")
     private fun onButtonStartPressed(): Triple<Int, Int, Char> {
         number_1 = Random.nextInt(10, 100)
         number_2 = Random.nextInt(10, 100)
@@ -54,7 +58,6 @@ class MainActivity : AppCompatActivity() {
         binding.txtOperation.text = operation.toString()
         binding.txtFirstOperand.text = number_1.toString()
         binding.txtSecondOperand.text = number_2.toString()
-        // buttonStart.isEnabled = false
 
         probability = Random.nextBoolean();
         if (probability) {
@@ -69,23 +72,54 @@ class MainActivity : AppCompatActivity() {
                         textResult.text = decFormat.format(number_1.toDouble() / number_2.toDouble())
                     }
                 }
-                else -> textResult.text = "Error"
             }
         }
         else {
-            result = Random.nextInt(10, 9801)
+            result = Random.nextInt(-99, 9801)
             textResult.text = result.toString()
         }
-
-
+        buttonStart.isEnabled = false
+        buttonRight.isEnabled = true
+        buttonWrong.isEnabled = true
         return Triple(number_1, number_2, operation)
     }
 
     private fun onButtonRightPressed() {
-
+        val answer = check()
+        if (answer == right)
+            txtRightAnswer.text = answer.toString()
+        else
+            txtWrongAnswer.text = answer.toString()
     }
 
     private fun onButtonWrongPressed() {
+        val answer = check()
+        if (answer == wrong)
+            txtRightAnswer.text = answer.toString()
+        else
+            txtWrongAnswer.text = answer.toString()
 
+    }
+
+    private fun check(): Int  {
+        var answer = 0
+        if ((operation == '+' && textResult.text == (number_1 + number_2).toString()) ||
+            (operation == '-' && textResult.text == (number_1 - number_2).toString()) ||
+            (operation == '*' && textResult.text == (number_1 * number_2).toString()) ||
+            (operation == '/' && ((number_1 % number_2 == 0 && textResult.text == (number_1 / number_2).toString()) ||
+                    (number_1 % number_2 != 0 && textResult.text == decFormat.format(number_1.toDouble() / number_2.toDouble()))))
+            ) {
+            right++
+            answer = right
+        }
+        else {
+            wrong++
+            answer = wrong
+        }
+        buttonRight.isEnabled = false
+        buttonWrong.isEnabled = false
+        buttonStart.isEnabled = true
+
+        return answer
     }
 }
